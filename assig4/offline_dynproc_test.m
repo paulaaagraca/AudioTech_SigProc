b81ed7577c8b7258deabea4df118dde1c47d98e9
paulaaagraca <paulagraca97@gmail.com>
2019-10-24 00:27:06 +0100

    Update offline_dynproc_test.m

--- assig4/offline_dynproc_test.m
+++ assig4/offline_dynproc_test.m
@@ -1,4 +1,7 @@
-
+%
+% Author : Paula A A Graça
+% Student @ TUM 2019
+%
 [sig,fs] = audioread('AIP_Song.wav');
 
 v = offline_dynproc(sig, 10, -20,    -30,     -80,     2,   500, 100, 300, 0,      fs);
